efa8fb15ece3780f1f803894285620f1d9aaad33
foster <fosternilsson99@gmail.com>
2024-05-20 12:40:27 +0200

    switched to postgres

--- app/db.py
+++ app/db.py
@@ -1,41 +1,20 @@
-from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, scoped_session
-from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.dialects.mysql import pymysql
-from sqlalchemy.dialects import mysql
 import os
 from dotenv import load_dotenv
+
 
 load_dotenv('.env.prod') 
 
 DATABASE_URL = os.getenv('DATABASE_URL')
 
-# Explicitly create engine with PyMySQL dialect
-engine = create_engine(
-    DATABASE_URL,
-    connect_args={'ssl_ca':"/etc/ssl/certs/ca-certificates.crt"},
-    dialect=mysql.dialect()  # Explicitly set the dialect
-)
-db = SQLAlchemy(app)
+engine = create_engine(DATABASE_URL)
 
-Session = scoped_session(sessionmaker(autocommit=False,
-                                         autoflush=False,
-                                         bind=engine))
-Base = declarative_base()
-
-class Response(db.Model):
-    __tablename__ = 'responses'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(255), nullable=False)
-    answers = db.Column(db.Text, nullable=False)
-
-def init_db(app):
-    db.init_app(app)
-    with app.app_context():
-        db.create_all() 
+from sqlalchemy import text
 
 def store_answers(data):
-    new_response = Response(email=data['Email'], answers=str(data))
-    db.session.add(new_response)
-    db.session.commit()
+    insert_query = text(
+        "INSERT INTO responses (email, answers) VALUES (:email, :answers)"
+    )
+
+    with engine.connect() as connection:
+        connection.execute(insert_query, email=data['Email'], answers=str(data))
