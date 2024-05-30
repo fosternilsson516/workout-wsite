0000fa4ed504fbdf4a3ddddcfd298dd9b380b7cb
foster <fosternilsson99@gmail.com>
2024-05-30 19:52:26 +0200

    db

--- app/question_handler.py
+++ app/question_handler.py
@@ -1,15 +1,22 @@
 from app.db import db_connection
 import psycopg2
+import logging
 
 class UserQuestions:
     def __init__(self):
         self.conn = db_connection()
+        if self.conn is None:
+            logging.error("Failed to establish database connection.")
+        else:
+            logging.info("Database connection established successfully.")
 
     def set_answers(self, data):
-        if self.conn is None or self.conn.closed:
-            self.conn = db_connection()
+        if self.conn is None:
+            logging.error("Database connection is not established.")
+            raise ValueError("Database connection is not established")
         try:
             cursor = self.conn.cursor()
+            logging.info("Cursor created successfully.")
 
             cursor.execute("INSERT INTO responses (data) VALUES (%s)",
                             (data,))
